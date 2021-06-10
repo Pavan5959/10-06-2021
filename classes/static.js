1f4abc358347f48f3f5a163298ec0348efabcb5c
PAVAN R <82048445+Pavan5959@users.noreply.github.com>
2021-06-10 20:24:30 +0530

    Create static.js

---
+++ classes/static.js
@@ -0,0 +1,14 @@
+class Html{
+   constructor(name){
+     this.name = name;
+     console.log(this.name);
+   }
+}
+
+class Css {
+  static getMessage(message){
+    console.log(message);
+  
+}
+}
+Css.getMessage('this is es6 updated version of Javascript '); 
