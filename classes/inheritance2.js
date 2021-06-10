0f60d5138496f97fca27ffa977c994d04578d0ba
PAVAN R <82048445+Pavan5959@users.noreply.github.com>
2021-06-10 20:01:52 +0530

    Create inheritance2.js

---
+++ classes/inheritance2.js
@@ -0,0 +1,27 @@
+class Person{
+  constructor(name){
+    this.name = name;
+    console.log('just hello');
+  }
+  hello(){
+    console.log(`hello this is ${this.name} and my age is ${this.age}`);
+  }
+}
+
+class People extends Person{
+   constructor(age){
+     super('source ');
+     this.age = age;
+   }
+   hello()
+{
+  console.log("hello");
+}
+   helloTwice(){
+     this.hello();
+     this.hello();
+   }
+}
+
+let people = new People(20);
+people.helloTwice();
